8f148ac8800eae8ace7a2efb30b0a7e5a8aa4ec9
Patrick Thompson <204189+pthompson@users.noreply.github.com>
2022-10-16 16:10:08 -0700

    Add function docs

--- lib/luna_take_home/og_image_worker.ex
+++ lib/luna_take_home/og_image_worker.ex
@@ -8,8 +8,32 @@ defmodule LunaTakeHome.OGImageWorker do
 
   @topic inspect(__MODULE__)
 
+  @doc """
+  Subscribe to the OGImageWorker PubSub topic
+
+  OGImageWorker results will be published on the topic.
+
+  When the job completes a pubsub message with the results will be published
+  with the following success and failure formats:
+    Success: {__MODULE__, :ok, page_url, og_image_url}
+    Error: {__MODULE__, :error, page_url, reason}
+  """
+  @spec subscribe :: :ok | {:error, {:already_registered, pid}}
   def subscribe, do: Phoenix.PubSub.subscribe(LunaTakeHome.PubSub, @topic)
 
+  @doc """
+  Create Oban job to get og:image from web page meta element
+
+  When the job completes a pubsub message with the results will be published.
+  Subscribe to the pubsub @topic before invoking this function.
+
+  ## Parameters
+
+    - url: URL string of the target web page
+
+  """
+  @spec get_og_image(String.t()) ::
+          {:ok, Oban.Job.t()} | {:error, Oban.Job.changeset() | term()}
   def get_og_image(page_url) do
     __MODULE__.new(%{page_url: page_url})
     |> Oban.insert()
