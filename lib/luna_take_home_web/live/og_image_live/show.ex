4d33c6243a5632075541062ca7f253ad99ad343f
Patrick Thompson <204189+pthompson@users.noreply.github.com>
2022-10-17 23:54:18 -0700

    Get and display og_image in OGImageLive.Show
    - Subscribe to OGImageWorker topic when connected
    - When get validated url, call OGImageWorker to fetch the og_image
    - Listen for pubsub response in handle_info functions, and
    - Display image or error message from pubsub message

--- lib/luna_take_home_web/live/og_image_live/show.ex
+++ lib/luna_take_home_web/live/og_image_live/show.ex
@@ -1,42 +1,86 @@
 defmodule LunaTakeHomeWeb.OGImageLive.Show do
   use LunaTakeHomeWeb, :live_view
+  alias LunaTakeHome.OGImageWorker
   alias LunaTakeHomeWeb.Util
 
   @impl true
   def mount(_params, _session, socket) do
+    if connected?(socket) do
+      OGImageWorker.subscribe()
+    end
+
     {:ok, socket}
   end
 
   @impl true
   def handle_params(_, _, socket) do
     changeset = changeset(%{})
-    og_image_request = %{url: nil}
 
     {:noreply,
      socket
      |> assign(
        page_title: "OG Image Viewer",
        changeset: changeset,
-       og_image_request: og_image_request
+       url: nil,
+       og_image_url: nil,
+       fetching?: false,
+       fetch_error: nil
      )}
   end
 
   @impl true
   def handle_event("get_og_image", %{"og_image_request" => params}, socket) do
-    og_image_request = socket.assigns.og_image_request
-
-    changeset = changeset(og_image_request, params)
+    changeset = changeset(%{}, params)
 
     case Ecto.Changeset.apply_action(changeset, :update) do
       {:ok, _request} ->
-        # TODO: get the og image
-        {:noreply, assign(socket, :changeset, changeset)}
+        url = changeset.changes.url
+        OGImageWorker.get_og_image(url)
+
+        {:noreply,
+         assign(socket,
+           changeset: changeset,
+           url: url,
+           og_image_url: nil,
+           fetching?: true,
+           fetch_error: nil
+         )}
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        {:noreply, assign(socket, :changeset, changeset)}
+        {:noreply,
+         assign(socket,
+           changeset: changeset,
+           url: nil,
+           og_image_url: nil,
+           fetching?: false,
+           fetch_error: nil
+         )}
     end
   end
 
+  @impl Phoenix.LiveView
+  # Handle pubsub event with result of get_og_image/1 call to OGImageWorker.
+  # Filter by url.
+  def handle_info(
+        {LunaTakeHome.OGImageWorker, :ok, url, og_image_url},
+        %{assigns: %{url: url, fetching?: true}} = socket
+      ) do
+    {:noreply, assign(socket, og_image_url: og_image_url, fetch_error: nil, fetching?: false)}
+  end
+
+  def handle_info(
+        {LunaTakeHome.OGImageWorker, :error, url, reason},
+        %{assigns: %{url: url, fetching?: true}} = socket
+      ) do
+    {:noreply, assign(socket, fetch_error: reason, og_image_url: nil, fetching?: false)}
+  end
+
+  # Ignore other messages
+  def handle_info(_message, socket) do
+    {:noreply, socket}
+  end
+
+  # Schemaless changeset
   defp changeset(%{} = request, params \\ %{}) do
     types = %{url: :string}
 
